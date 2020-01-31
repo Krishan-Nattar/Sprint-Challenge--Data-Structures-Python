91e5c1224df79e4da699ba03b18d6bb510c8fe17
Krishan Nattar <krishan.nattar@gmail.com>
2020-01-31 08:31:41 -0800

    commented runtime complexities

--- names/names.py
+++ names/names.py
@@ -3,10 +3,12 @@ from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
+# O(n)
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# O(n)
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -22,9 +24,16 @@ for name_2 in names_2: # O(n)
     if binary_tree.contains(name_2): # O(log n)
         duplicates.append(name_2)
 
+# Runtime: 0.06696081161499023 seconds
+# Runtime complexity: O(2n log n)
+# n = input size of names. (They are different inputs, but i simplified it to one 'n' variable instead of n1 + n2, because they are both at 10,000 names)
+
+
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
